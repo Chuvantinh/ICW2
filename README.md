62c5fd952dca13d62545f4e5c6b9d89cc0cb9972
Chuvantinh <chuvantinh1991@gmail.com>
2020-11-08 20:41:42 +0100

    add report file text for prduct quantization

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Wintersemester 2020-2021
 Van Tinh Chu
 s067851@htw-berlin.de
 date: 08.11.2020
+# Histoy of the meeting between Professor and Van Tinh
+04,11,2020
+Research Product Quantization to optimize the time of all algorithmen.
+
+This method will divide the big dataset to subspace. Eg: 100 dimension into 5 times 20 Dimension
+ In this project I'm using the library pqkmeans to train e dataset to small dataset.
+ 
+Result is then time of processing PQKmean very short in the comparison with Kmeans
--- venv/src/ICW2/product quantization method icw/product_quantization.py
+++ venv/src/ICW2/product quantization method icw/product_quantization.py
@@ -1,20 +1,50 @@
 import pqkmeans
 import numpy as np
-X = np.random.random((1000, 128)) # 128 dimensional 100,000 samples
-print(X)
+from sklearn.cluster import KMeans
+# for data set
+from sklearn.datasets.samples_generator import make_blobs
+# time measure
+import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
+
+n_points_per_cluster_total = 100000
+size_colum = 100
+centers = np.random.randint(-20, 20, size=(size_colum,size_colum))
+
+X, labels_true = make_blobs(n_samples=n_points_per_cluster_total,
+                            centers=centers,
+                            n_features=size_colum,
+                            cluster_std=0.4,
+                            random_state=0)
+print(X.shape)
+
 # Train a PQ encoder.
 # Each vector is divided into 4 parts and each part is
 # encoded with log256 = 8 bit, resulting in a 32 bit PQ code.
 encoder = pqkmeans.encoder.PQEncoder(num_subdim=4, Ks=256)
-encoder.fit(X[:1000])  # Use a subset of X for training
+encoder.fit(X)  # Use a subset of X for training
 
 # Convert input vectors to 32-bit PQ codes, where each PQ code consists of four uint8.
 # You can train the encoder and transform the input vectors to PQ codes preliminary.
 X_pqcode = encoder.transform(X)
-
+print(X_pqcode)
 # Run clustering with k=5 clusters.
-kmeans = pqkmeans.clustering.PQKMeans(encoder=encoder, k=5)
-clustered = kmeans.fit_predict(X_pqcode)
+db_time_pqkmean = time.time()
+pqkmeans_cluster = pqkmeans.clustering.PQKMeans(encoder=encoder, k=100)
+clustered = pqkmeans_cluster.fit_predict(X_pqcode)
+db_time_pqkmean_process = time.time() - db_time_pqkmean
+print('Number of cluster in PQ Kmeans :  % s ' % len(pqkmeans_cluster.cluster_centers_))
+print('Elapsed time to cluster in PQ Kmeans :  %.4f s ' % db_time_pqkmean_process)
+
+db_time = time.time()
+kmeans = KMeans(n_clusters=100, random_state=0, n_jobs=-1).fit(X)
+kmeans.predict(X)
+
+db_time_process = time.time() - db_time
+print('Number of cluster in  Kmeans :  % s ' % len(kmeans.cluster_centers_))
+print('Elapsed time to cluster in kmeans :  %.4f s ' % db_time_process)
 
 # Then, clustered[0] is the id of assigned center for the first input PQ code (X_pqcode[0]).
 
