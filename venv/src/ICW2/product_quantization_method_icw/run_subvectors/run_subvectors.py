1d188ca82ccb8c45e5ff25cae16f02998f897223
Chuvantinh <chuvantinh1991@gmail.com>
2021-01-06 20:48:32 +0100

    run subvector with kmeans and mini k

--- venv/src/ICW2/product_quantization_method_icw/run_subvectors/run_subvectors.py
+++ venv/src/ICW2/product_quantization_method_icw/run_subvectors/run_subvectors.py
@@ -10,8 +10,8 @@ import time
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
-
-n_points_per_cluster_total = 2000000
+time_all = time.time()
+n_points_per_cluster_total = 100000
 size_colum = 100
 centers = np.random.randint(-20, 20, size=(size_colum,size_colum))
 #print('centers', centers[0])
@@ -38,7 +38,7 @@ print('###############################################')
 ###############
 from sklearn.cluster import KMeans
 # Try X1 with kmeans
-kmeans = KMeans(n_clusters=100, random_state=0).fit(X1)
+kmeans = KMeans(n_clusters=50, random_state=0).fit(X1)
 centers = kmeans.cluster_centers_
 print('centers', centers)
 print('kmeans.labels_', kmeans.labels_[:10])
@@ -46,20 +46,25 @@ print('kmeans.labels_ len', len(kmeans.labels_))
 print('###############################################')
 
 # try X2 with dbscan
-from sklearn.cluster import DBSCAN
-clustering = DBSCAN(eps=15, min_samples=2, n_jobs=-1).fit(X2)
-
-print('dbscan clustering.labels_', clustering.labels_[50:60])
-print('dbscan clustering.labels_ len', len(clustering.labels_))
-labels = clustering.labels_
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-print('X1 : Number of cluster in DBSCANN :  % d ' % n_clusters_)
+# from sklearn.cluster import DBSCAN
+# clustering = DBSCAN(eps=15, min_samples=2, n_jobs=-1).fit(X2)
+#
+# print('dbscan clustering.labels_', clustering.labels_[50:60])
+# print('dbscan clustering.labels_ len', len(clustering.labels_))
+# labels = clustering.labels_
+# n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+# print('X1 : Number of cluster in DBSCANN :  % d ' % n_clusters_)
 # print('X1: Elapsed time to cluster in DBSCANN :  %.4f s ' % db_time_dbscan_process)
+kmeans2 = KMeans(n_clusters=50, random_state=0).fit(X2)
+centers2 = kmeans2.cluster_centers_
+print('centers of kmeans 2', centers2)
+print('kmeans2.labels_', kmeans2.labels_[:10])
+print('kmeans2.labels_ len', len(kmeans2.labels_))
 print('###############################################')
 
-# Optics with X3
+# Optics with X
 from sklearn.cluster import OPTICS
-optics = OPTICS(min_samples=10, xi=.05, min_cluster_size=.05, n_jobs=-1).fit(X3)
+optics = OPTICS(min_samples=10, xi=.05, min_cluster_size=.05, n_jobs=-1).fit(X)
 print('Optics clustering labels', optics.labels_)
 print(' Len Optics clustering labels', len(optics.labels_))
 op_labels = optics.labels_
@@ -67,11 +72,20 @@ print('Optics labels :', op_labels)
 n_clusters_op_ = len(set(op_labels)) - (1 if -1 in op_labels else 0)
 print('Number CLuster of Optics is ', n_clusters_op_)
 print('optics The cluster ordered list of sample indices. ', optics.ordering_)
+
+# Try X3 with kmeans
+kmeans3 = KMeans(n_clusters=50, random_state=0).fit(X3)
+centers3 = kmeans3.cluster_centers_
+print('centers of kmeans 3', centers3)
+print('kmeans3.labels_', kmeans3.labels_[:10])
+print('kmeans3.labels_ len', len(kmeans3.labels_))
+print('###############################################')
+
 print('###############################################')
 
 from sklearn.cluster import MiniBatchKMeans
 db_time_minibatch = time.time()
-_MiniBatchKMeans = MiniBatchKMeans(n_clusters=100,random_state = 0,batch_size = 300, max_iter = 100).fit(X4)
+_MiniBatchKMeans = MiniBatchKMeans(n_clusters=50,random_state = 0,batch_size = 300, max_iter = 100).fit(X4)
 
 db_time_process = time.time() - db_time_minibatch
 print('Elapsed time to cluster in MInibatch kmeans :  %.4f s ' % db_time_process)
@@ -79,13 +93,22 @@ cluster_MiniBatchKMeans = _MiniBatchKMeans.cluster_centers_
 labels_MiniBatchKMeans = _MiniBatchKMeans.labels_
 print('labels_MiniBatchKMeans',labels_MiniBatchKMeans)
 print('###############################################')
-total_labels = kmeans.labels_ + clustering.labels_ + op_labels + labels_MiniBatchKMeans
-print('total_labels',total_labels)
+total_labels = kmeans.labels_ + kmeans2.labels_ + kmeans3.labels_ + labels_MiniBatchKMeans
+print('total_labels',len(total_labels))
 # find same element in list
 import collections
 same_element = [item for item, count in collections.Counter(total_labels).items() if count > 1]
-differences_element = [item for item, count in collections.Counter(total_labels).items() if count == 1]
+# differences_element = [item for item, count in collections.Counter(total_labels).items() if count == 1]
+
 print('same_element',same_element)
-print('differences_element',differences_element)
+print('differences_element',len(total_labels) - len(same_element))
+
 print('len of total labels in init vector', len(same_element))
 # len of total labels in init vector 77 -> 77 cluster or what happend ?
+time_total =  time.time() - time_all
+print('time_total', time_total)
+# ergebnisse
+# 1mi , time all  len of total labels in init vector 80, time_total 286.71242094039917 s
+# 2 mi, len of total labels in init vector 79,  time_total 579.318208694458 = 9 min
+# 5 mi, len of total labels in init vector 82 # time_total 2588.9190237522125 = 43 min
+# 7 mi, len of total labels in init vector 79 # time_total 3572.8744649887085  = 1 hour
